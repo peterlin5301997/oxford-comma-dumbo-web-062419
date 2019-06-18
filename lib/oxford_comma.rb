cc8cb79e8e856813d67e3fcfd0c7caea2fb1eb69
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:42:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,7 @@ def oxford_comma(array)
   elsif array.size == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
+    output = ""
     for i in array do
       if i != array.size
         output += "#{i}, "
