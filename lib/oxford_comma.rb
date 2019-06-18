3ac0b14486a1841116a917cd96153327af88cc78
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:44:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return "#{array[0]}"
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    output = ""
+    for i in array do
+      if i != array[-1]
+        output += "#{i}, "
+      else
+        output += "and #{i}"
+      end
+    end
+  end
+  return output
 end
